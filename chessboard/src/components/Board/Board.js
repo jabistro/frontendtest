0be4a98a54f00864454f63e8c8f7439995f29148
John Allan Hinds <jabistro@gmail.com>
2023-05-25 15:30:47 -0400

    post interview suggestion

--- chessboard/src/components/Board/Board.js
+++ chessboard/src/components/Board/Board.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import Square from '../Square/Square'
-import { chessSquareNames } from '../../utils/chessSquareNames';
-import './Board.css';
+import React, { useEffect, useState } from "react";
+import Square from "../Square/Square";
+import { chessSquareNames } from "../../utils/chessSquareNames";
+import "./Board.css";
 
-const Board = ({ logSquare, selectedSquare}) => {
-    const [board, setBoard] = useState([]);
+const Board = ({ logSquare, selectedSquare }) => {
+  const [board, setBoard] = useState([]);
 
-    useEffect(()=>{
-        setBoard(chessSquareNames());
-    }, []);
-    
-    return (
-        <div className='board'>
-            {board && board.map((square, index)=>{
-                return (
-                    <Square key={index} logSquare={logSquare} selectedSquare={selectedSquare} id={square} index={index} />
-                )
-            })}
-        </div>
-    )
-}
+  useEffect(() => {
+    setBoard(chessSquareNames());
+  }, []);
 
-export default Board
+  return (
+    <div className="board">
+      {board &&
+        board.map((square, index) => {
+          return (
+            <Square
+              key={index}
+              logSquare={logSquare}
+              selectedSquare={selectedSquare}
+              id={square}
+              index={index}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default Board;
--- chessboard/src/utils/chessSquareNames.js
+++ chessboard/src/utils/chessSquareNames.js
@@ -1,14 +1,15 @@
 export const chessSquareNames = () => {
-    let namesArr = [];
-    let letter = "a";
-    let number = 8;
-    for (let i = 1; i <= 64; i++) {
-      namesArr = [...namesArr, letter + number.toString()];
-      if (i % 8 === 0) {
-        number--;
-      }
-      if (letter === "h") letter = "a"
-      else letter = String.fromCharCode(letter.charCodeAt(0) + 1);
+  let namesArr = [];
+  let letter = "a";
+  let number = 8;
+  for (let i = 1; i <= 64; i++) {
+    // .push instead of creating new arr each time w/ spread
+    namesArr = [...namesArr, letter + number.toString()];
+    if (i % 8 === 0) {
+      number--;
     }
-    return namesArr;
-}
+    if (letter === "h") letter = "a";
+    else letter = String.fromCharCode(letter.charCodeAt(0) + 1);
+  }
+  return namesArr;
+};
